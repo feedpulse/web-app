6927029f32d2687ebf23c62d49b0a509a60bb64f
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-02-09 21:32:58 +0100

    draft: add 404 Not Found View and routing logic
    A new "NotFoundView" has been added to handle unfound paths. The routing logic has been updated to fall back to this newly added 404 view when an invalid route is accessed. This enhances user experience by providing a more meaningful screen for a non-existent page instead of rendering an empty or broken page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,7 @@ import LoginView from "@/views/LoginView.vue";
 import AboutView from "@/views/AboutView.vue";
 import SettingsView from "@/views/SettingsView.vue";
 import InfoSettingsView from "@/views/settings/InfoSettingsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -95,15 +96,11 @@ const router = createRouter({
                 }
             ],
         },
-        // {
-        //     path: '/:pathMatch(.*)*',
-        //     name: 'not-found',
-        //     component: () => import(/* webpackChunkName: "not-found" */ '../views/NotFoundView.vue'),
-        //     meta: {
-        //         layout: DefaultLayout,
-        //         middleware: [authMiddleware]
-        //     }
-        // }
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFoundView,
+        }
         // {
         //   path: '/about',
         //   name: 'about',
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="bg-surface-900 w-screen h-screen">
+        <div class="flex flex-col items-center justify-center h-full">
+            <h1 class="text-9xl text-primary-500">404</h1>
+            <h2 class="text-3xl text-white">Page not found</h2>
+            <!-- go home -->
+            <router-link to="/">
+                <button class="bg-primary-500 text-white px-4 py-2 rounded-md mt-8">Go Home</button>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
